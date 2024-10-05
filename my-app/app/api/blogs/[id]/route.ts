a4652de29480140f9c9c532084a93a218701a97c
fredy <tredy-94@outlook.com>
2024-10-05 10:01:05 -0500

    First commit

---
+++ my-app/app/api/blogs/[id]/route.ts
@@ -0,0 +1,81 @@
+import { deletePost, getById, updatePost } from "@/lib/data";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  // get a post by id
+
+  try {
+    const id = req.url.split("blogs/")[1];
+    console.log(id);
+    const post = getById(id);
+    if (!post) {
+      return NextResponse.json(
+        {
+          message: "ERROR",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    return NextResponse.json(
+      {
+        message: "OK",
+        post,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "ERROR",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
+export const PUT = async (req: Request) => {
+  // update a post by id
+
+  try {
+    const { title, desc } = await req.json();
+    const id = req.url.split("blogs/")[1];
+    updatePost(id, title, desc);
+
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
+export const DELETE = async (req: Request) => {
+  // Delete post by id
+    try {
+        const id = req.url.split("blogs/")[1]
+        deletePost(id)
+        return NextResponse.json({
+            message: "POST DELETED"
+        }, {status:200})
+    } catch (error) {
+        return NextResponse.json({
+            message: "ERROR", error
+        },
+        {
+            status: 500
+        }
+    )
+    }
+
+};
